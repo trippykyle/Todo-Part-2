90be4b639d7c17294e871adf45468f323f28417c
Trippykyle <kylerigsby10@yahoo.com>
2024-07-29 18:22:45 -0400

    created tasks controller,validations and new.html and updated index and routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,37 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:content)
+    end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,3 @@
 class Task < ApplicationRecord
+  validates :content, presence: true, length: { in: 2..50 }, uniqueness: true
 end
